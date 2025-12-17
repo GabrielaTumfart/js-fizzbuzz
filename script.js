4dbd898113d400d1664a9721044d85132741ed81
GabrielaTumfart <gabrielatumfart@gmail.com>
2025-12-17 13:21:19 +0100

    continuo a cercare la soluzione giusta coll'impostazione di default

--- script.js
+++ script.js
@@ -51,8 +51,17 @@ scrivi: Buzz
 
 // ? 4) ELABORAZIONE
 //i uguale a 1 perché voglio iniziare da 1
-for(let i = 1; i <= 100; i++);
-console.log(i);
+
+for (let i = 1; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+};
+
+
 
 
 
