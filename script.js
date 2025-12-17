57ed56591ce7751c826a746959dcb1edd50388ba
GabrielaTumfart <gabrielatumfart@gmail.com>
2025-12-17 15:16:10 +0100

    riprendo ora

--- script.js
+++ script.js
@@ -56,29 +56,30 @@ scrivi: Buzz
 
 
 
-//i uguale a 1 perché voglio iniziare da 1
+// i uguale a 1 perché voglio iniziare da 1
 for (let i = 1; i <= 100; i++) {
     console.log(i);
-}
 
-if (i % 15 === 0) {
+    if (i % 15 === 0) {
 
-    console.log("FizzBuzz");
+        console.log("FizzBuzz");
 
-    else if (i % 3 === 0) {
+    } else if (i % 3 === 0) {
 
         console.log("Fizz");
 
-    else if (i % 5 === 0) {
+    } else if (i % 5 === 0) {
 
         console.log("Buzz");
-        
+
     } else {
 
         console.log(i);
     }
-    
+
 } 
+}
+
 
 
 
